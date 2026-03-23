3a6b16bdb2048ded76c11c8b2cdae9ef5d9f3972
luynt <nguyenthanhluynd@gmail.com>
2026-03-23 23:10:39 +0700

    add alias type

--- weed.go
+++ weed.go
@@ -2,9 +2,16 @@ package weed
 
 import (
 	"fmt"
-	"github.com/wdvn/weed/internal/core/http"
 	std "net/http"
+
+	"github.com/wdvn/weed/core/http"
 )
+
+// Ctx is an alias for http.Ctx so external projects can use weed.Ctx
+type Ctx = http.Ctx
+
+// HandlerFunc is an alias for the router handler function
+type HandlerFunc = http.HandlerFunc
 
 type App struct {
 	router *http.Router
@@ -26,4 +33,29 @@ func (app *App) Serve(port string) error {
 
 func (app *App) Use(middle http.MiddlewareFunc) {
 	app.router.Use(middle)
+}
+
+// GET is a shortcut for router.GET
+func (app *App) GET(path string, handler HandlerFunc) {
+	app.router.GET(path, handler)
+}
+
+// POST is a shortcut for router.POST
+func (app *App) POST(path string, handler HandlerFunc) {
+	app.router.POST(path, handler)
+}
+
+// PUT is a shortcut for router.PUT
+func (app *App) PUT(path string, handler HandlerFunc) {
+	app.router.PUT(path, handler)
+}
+
+// DELETE is a shortcut for router.DELETE
+func (app *App) DELETE(path string, handler HandlerFunc) {
+	app.router.DELETE(path, handler)
+}
+
+// Group creates a new router group
+func (app *App) Group(prefix string, middlewares ...http.MiddlewareFunc) *http.RouterGroup {
+	return app.router.Group(prefix, middlewares...)
 }
